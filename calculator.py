60b5493513ae07dcfb0f76cf85d0d76907271c77
Thế Sơn <nguyentheson27102005@gmail.com>
2025-07-05 11:00:34 +0700

    change

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 # calculator.py
 
-def add(a, b):
-    return a + b
-
 def subtract(a, b):
     return a - b
+
+def add(a, b):
+    return a + b
 
 if __name__ == "__main__":
     print("3 + 2 =", add(3, 2))
